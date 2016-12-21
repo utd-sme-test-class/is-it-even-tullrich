256888b08ea356d3fcd38a70898be1ecbefb2989
Georgia Stuart <georgia@Georgias-MacBook-Pro.local>
2016-12-20 23:02:46 -0600

    exercise

---
+++ ex_02.js
@@ -0,0 +1,21 @@
+/*
+Assignment: Write a function USING RECURSION that takes an integer input and returns true if the
+number is even and false if the number is odd.
+ */
+
+var is_even = function (num) {
+//    Your code here
+};
+
+// TESTING
+
+if (!is_even(12))
+    console.log('Fails on 12');
+if (is_even(13))
+    console.log('Fails on 13');
+if (!is_even(0))
+    console.log('Fails on 0');
+if (is_even(1))
+    console.log('Fails on 1');
+if (is_even(-1))
+    console.log(('Fails on -1'));
